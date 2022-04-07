e7b33ec463f4fe216caf9bb72d63b1b80e1175d0
Сергей Щикунов <admod32@gmail.com>
2022-04-07 21:55:20 +0300

    Add code change name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let name = "Сергей";
+
+  const handleClick = () => {
+    name = "Екатерина";
+    console.log(name);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Меня зовут {name}</h1>
+      <button onClick={handleClick}>Сменить имя</button>
     </div>
   );
 }
