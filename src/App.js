fbb9d0bc159711c2b3a5cde9b54410caffb6b7de
Сергей Щикунов <admod32@gmail.com>
2022-04-08 20:58:56 +0300

    Change code in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { useState } from "react";
 
 function App() {
   const [name, setName] = useState("Сергей");
+  const [events, setEvents] = useState([
+    { title: "гулянка на ДР Сергея", id: 1 },
+    { title: "буза в прямом эфире", id: 2 },
+    { title: "гонки на коровьей ферме", id: 3 },
+  ]);
 
   const handleClick = () => {
     setName("Екатерина");
@@ -13,6 +18,11 @@ function App() {
     <div className="App">
       <h1>Меня зовут {name}</h1>
       <button onClick={handleClick}>Сменить имя</button>
+      {events.map((event) => (
+        <div key={event.id}>
+          <h2>{event.title}</h2>
+        </div>
+      ))}
     </div>
   );
 }
