4df6f6ca36621db1a6c7d689df7e68c8cdc737f0
Сергей Щикунов <admod32@gmail.com>
2022-04-09 21:04:09 +0300

    Change code in App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("Сергей");
   const [events, setEvents] = useState([
     { title: "гулянка на ДР Сергея", id: 1 },
     { title: "буза в прямом эфире", id: 2 },
     { title: "гонки на коровьей ферме", id: 3 },
   ]);
 
-  const handleClick = () => {
-    setName("Екатерина");
-    console.log(name);
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id;
+      });
+    });
+    console.log(id);
   };
 
   return (
     <div className="App">
-      <h1>Меня зовут {name}</h1>
-      <button onClick={handleClick}>Сменить имя</button>
       {events.map((event) => (
         <div key={event.id}>
           <h2>{event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>Удалить событие</button>
         </div>
       ))}
     </div>
